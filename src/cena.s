d2cc4eb90f656b3dfd1a747927566961b74f687b
gugawcosta <gustavowallacecosta@gmail.com>
2025-03-26 17:46:48 +0000

    some changes

--- src/cena.s
+++ src/cena.s
@@ -91,20 +91,13 @@ umull32:
     POP R4
     POP PC
 
-main:
-    MOV R0, #0
-    MOV R1, #50
-    MOV R2, #0
-    MOV R3, #50
-    BL umull32
-    
 ; nseed -> R0 e R1
 ; seed_addr -> R4
 srand:
     PUSH R4
     LDR R4, seed_addr
     STR R0, [R4]
-    STR R1, [R4, #2]
+    STR R1, [R4, #4]
     POP R4
     MOV PC, LR
 
@@ -118,23 +111,27 @@ rand:
     ; seed atual (32 bits) -> R0 e R1
     LDR R4, seed_addr
     LDR R0, [R4]    ; Parte inferior
-    LDR R1, [R4, #2]    ; Parte superior
+    LDR R1, [R4, #4]    ; Parte superior
 
     ; prepara para umull32: seed * 214013
     ; 214013 = 0x000343FD (32 bits)
-    MOV R2, #0x43FD   ; Parte inferior
-    MOV R3, #0x0003   ; Parte superior
+    MOV R2, #0xFD   ; Parte inferior
+    MOVT R2, #0x43  ; Parte superior
+    MOV R3, #0x03   ; Parte inferior
     BL umull32  ; seed * 214013
 
     ; adiciona 2531011 (0x00269EC3) ao resultado
     ; para 64 bits, é preciso carry
-    ADD R0, R0, #0x9EC3     ; + parte inferior
-    ADC R1, R1, #0x0026     ; + parte superior com carry
+    MOV R2, #0xC3   ; Parte inferior
+    MOVT R2, #0x9E  ; Parte superior
+    MOV R3, #0x26   ; Parte inferior
+    ADD R0, R0, R2
+    ADC R1, R1, R3
 
     ; atualiza seed
     LDR R4, seed_addr
     STR R0, [R4]    ; Parte inferior
-    STR R1, [R4, #2]    ; Parte superior
+    STR R1, [R4, #4]    ; Parte superior
 
     ; retorna o resultado (seed >> 16)
     MOV R0, R1      ; retorna os 16 bits superiores
@@ -143,9 +140,15 @@ rand:
     POP R3
     POP R2
     POP R1
-
-    ADD SP, SP, #2  ; SP += 2 (para pular o LR)
+    POP R0
     POP PC          ; retorna
+
+main:
+    MOV R0, #0
+    MOV R1, #50
+    MOV R2, #0
+    MOV R3, #50
+    BL umull32
 
 seed_addr:
     .word seed
@@ -168,12 +171,17 @@ seed:
     
 /*
 214013:
-    .word 0x43FD    ; Parte inferior
-    .word 0x0003    ; Parte superior
+    .word 0x43_FD    ; Parte inferior
+    .word 0x00_03    ; Parte superior
 */
 /*
 2531011:
-    .word 0x9EC3    ; Parte inferior
-    .word 0x0026    ; Parte superior
+    .word 0x9E_C3    ; Parte inferior
+    .word 0x00_26    ; Parte superior
 */
 ; verificar RAND_MAX para 32 bits (0xFFFFFFFF), como fica?
+RAND_MAX:
+    .word 0xFFFF
+
+    
+
